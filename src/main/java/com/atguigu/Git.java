9e165297c5c3b3ce61ec608563c90364a4e9c624
layne <Layne1614912120@qq.com>
2022-06-10 10:25:14 +0800

    master commit2

--- src/main/java/com/atguigu/Git.java
+++ src/main/java/com/atguigu/Git.java
@@ -11,5 +11,6 @@ public class Git {
         System.out.println("hello github");
         System.out.println("hello git3");
         System.out.println("hello git4");
+        System.out.println("master test");
     }
 }
