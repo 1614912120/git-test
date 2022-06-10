32a240e366d9e75b78110948c1e924a7bc610c08
layne <Layne1614912120@qq.com>
2022-06-10 10:11:15 +0800

    third commit

--- src/main/java/com/atguigu/Git.java
+++ src/main/java/com/atguigu/Git.java
@@ -9,5 +9,6 @@ public class Git {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("hello github");
+        System.out.println("hello git3");
     }
 }
