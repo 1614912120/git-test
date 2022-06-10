190b4bff0e229f286178a7987fd17561c72498c0
layne <Layne1614912120@qq.com>
2022-06-10 10:10:18 +0800

    second commit

--- src/main/java/com/atguigu/Git.java
+++ src/main/java/com/atguigu/Git.java
@@ -8,5 +8,6 @@ package com.atguigu;
 public class Git {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello github");
     }
 }
