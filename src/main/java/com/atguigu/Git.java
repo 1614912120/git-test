909941b6ae0b3d2ca68ced0b52b4a61aa2deee0f
layne <Layne1614912120@qq.com>
2022-06-10 10:07:58 +0800

    first commit

---
+++ src/main/java/com/atguigu/Git.java
@@ -0,0 +1,12 @@
+package com.atguigu;
+
+/**
+ * @author 灰太狼
+ * @version 1.0
+ */
+
+public class Git {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
