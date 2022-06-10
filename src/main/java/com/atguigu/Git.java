26915029c0abe9018d46b805b68899bad6bf67b7
layne <Layne1614912120@qq.com>
2022-06-10 11:49:40 +0800

    push commit

--- src/main/java/com/atguigu/Git.java
+++ src/main/java/com/atguigu/Git.java
@@ -14,6 +14,7 @@ public class Git {
         System.out.println("master test");
         System.out.println("hot-fix test");
         System.out.println("hot-fix test");
+        System.out.println("hot-fix test");
     }
 
 }
