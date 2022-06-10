9b59669cf1337c63f8e8bcd029e1273e73f9d4ee
layne <Layne1614912120@qq.com>
2022-06-10 10:23:46 +0800

    hot-fix commit2

--- src/main/java/com/atguigu/Git.java
+++ src/main/java/com/atguigu/Git.java
@@ -11,5 +11,7 @@ public class Git {
         System.out.println("hello github");
         System.out.println("hello git3");
         System.out.println("hello git4");
+        System.out.println("hot-fix test");
     }
+
 }
